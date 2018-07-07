4d25e2c1edcadf2a667576642d4ac0b0a96886e7
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-07-07 16:13:26 +0530

    feat: compress images + responsive

--- ResponsiveImages/Gruntfile.js
+++ ResponsiveImages/Gruntfile.js
@@ -1,55 +1,41 @@
 /*
- After you have changed the settings at "Your code goes here",
+ After you have changed any settings for the responsive_images task,
  run this with one of these options:
   "grunt" alone creates a new, completed images directory
   "grunt clean" removes the images directory
   "grunt responsive_images" re-processes images without removing the old ones
 */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     responsive_images: {
       dev: {
         options: {
-          sizes: [{
-            name: 'small',
-            width: '30%',
-            suffix: '_small',
-            quality: 20
-          },{
-            name: 'large',
-            width: '50%',
-            suffix: '_large',
-            quality: 40
-          }]
+          sizes: [
+            {
+              width: 1600,
+              suffix: '_large_2x',
+              quality: 30
+            }
+          ]
         },
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images/',
-          dest: 'images/'
-        }]
-        },
-
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
-        }]
+        files: [
+          {
+            expand: true,
+            src: ['*.{gif,jpg,png}'],
+            cwd: 'images_src/',
+            dest: 'images/'
+          }
+        ]
       }
-    
+    },
 
     /* Clear out the images directory if it exists */
     clean: {
       dev: {
-        src: ['images'],
-      },
+        src: ['images']
+      }
     },
 
     /* Generate the images directory if it is missing */
@@ -57,23 +43,25 @@ module.exports = function(grunt) {
       dev: {
         options: {
           create: ['images']
-        },
-      },
+        }
+      }
     },
 
     /* Copy the "fixed" images that don't go through processing into the images/directory */
     copy: {
       dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-},
-},
-);
-  
+        files: [
+          {
+            expand: true,
+            src: ['images_src/fixed/*.{gif,jpg,png}'],
+            dest: 'images/',
+            flatten: true
+          }
+        ]
+      }
+    }
+  });
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
